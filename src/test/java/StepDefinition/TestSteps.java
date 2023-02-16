191504cd6ca64a40aad00460b2ae3ab1aea3d209
Mikhail Myasnikov <mmk_kz@mail.ru>
2023-02-16 20:55:36 +0700

    edit text

--- src/test/java/StepDefinition/TestSteps.java
+++ src/test/java/StepDefinition/TestSteps.java
@@ -3,20 +3,20 @@ package StepDefinition;
 import io.cucumber.java.ru.Когда;
 
 public class TestSteps {
-    @Когда("^Пользователь вводит x$")
+    @Когда("^Пользователь рандомно получает x$")
     public static float getX(){
         float x = (float) Math.random() * 8 + 1;
         System.out.println(x);
         return x;
     }
 
-    @Когда("^Пользователь вводит y$")
+    @Когда("^Пользователь рандомно получает y$")
     public static float getY(){
         float y = (float) Math.random() * 8 + 1;
         System.out.println(y);
         return y;
     }
-    @Когда("^Пользователь получает знак арифметического действия$")
+    @Когда("^Пользователь рандомно получает знак арифметического действия$")
     public  static String getSign(){
         String[] signs = new String[]{"+","-","/","*"};
         String sign = signs[(int) (Math.random() * 4)];
