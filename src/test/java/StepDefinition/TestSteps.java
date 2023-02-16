2531e98dc9c24480b7f8cd1e83d04244e313d4cc
Mikhail Myasnikov <mmk_kz@mail.ru>
2023-02-16 20:49:44 +0700

    create Calculator

---
+++ src/test/java/StepDefinition/TestSteps.java
@@ -0,0 +1,56 @@
+package StepDefinition;
+
+import io.cucumber.java.ru.Когда;
+
+public class TestSteps {
+    @Когда("^Пользователь вводит x$")
+    public static float getX(){
+        float x = (float) Math.random() * 8 + 1;
+        System.out.println(x);
+        return x;
+    }
+
+    @Когда("^Пользователь вводит y$")
+    public static float getY(){
+        float y = (float) Math.random() * 8 + 1;
+        System.out.println(y);
+        return y;
+    }
+    @Когда("^Пользователь получает знак арифметического действия$")
+    public  static String getSign(){
+        String[] signs = new String[]{"+","-","/","*"};
+        String sign = signs[(int) (Math.random() * 4)];
+        System.out.println(sign);
+        return sign;
+    }
+    @Когда("^Получить результат арифметического действия$")
+    public  static float getResultOperation(){
+        float x = getX();
+        float y = getY();
+        float result = 0;
+        switch(getSign()){
+            case ("+"):
+                result = x + y;
+                break;
+            case("-"):
+                result = x - y;
+                break;
+            case("*"):
+                result = x * y;
+                break;
+            case("/"):
+                result = x / y;
+                break;
+
+        }
+        return result;
+    }
+
+    @Когда("^Получить результат$")
+    public  static void getResult(){
+        float result = getResultOperation();
+        System.out.println(result);
+    }
+
+
+}
