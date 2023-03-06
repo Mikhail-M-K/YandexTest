108ba4f996db8528d2d793feea1e1a213aaf132a
Mikhail Myasnikov <mmk_kz@mail.ru>
2023-03-06 22:05:27 +0700

    убраны лишние импорты

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -1,24 +1,8 @@
-import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.cucumber.java.After;
-import io.cucumber.java.Scenario;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.AfterClass;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
